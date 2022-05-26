c8b932c58886742b802655eeaaf995bb7145f20e
Cristian Romero <cristian.romero@softwareestrategico.com>
2022-05-25 23:38:44 -0500

    Config login with amplify

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,9 @@
 import '../styles/globals.css'
+import { Amplify } from 'aws-amplify'
 import type { AppProps } from 'next/app'
+import { awsExports } from './../src/config/env'
+
+Amplify.configure(awsExports)
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
---
+++ src/config/env/index.ts
@@ -0,0 +1,4 @@
+export * from './development/awsExports'
+/**
+ * todo - add production env with imported dynamic variables
+ */
