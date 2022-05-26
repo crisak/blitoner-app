7097a6995f2d992f1c48f8ed4315765380fe766a
Cristian Romero <cristian.romero@softwareestrategico.com>
2022-05-25 23:56:36 -0500

    Create dark theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,35 @@ import '../styles/globals.css'
 import { Amplify } from 'aws-amplify'
 import type { AppProps } from 'next/app'
 import { awsExports } from './../src/config/env'
+import { NextUIProvider, createTheme } from '@nextui-org/react'
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
 
 Amplify.configure(awsExports)
 
+const lightTheme = createTheme({
+  type: 'light',
+})
+
+const darkTheme = createTheme({
+  type: 'dark',
+})
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <NextThemesProvider
+      defaultTheme="system"
+      attribute="class"
+      value={{
+        light: lightTheme.className,
+        dark: darkTheme.className
+      }}
+    >
+      <NextUIProvider>
+        <Component {...pageProps} />
+      </NextUIProvider>
+    </NextThemesProvider>
+
+  )
 }
 
 export default MyApp
